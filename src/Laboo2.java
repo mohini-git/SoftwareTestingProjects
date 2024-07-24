0fc7ebfe5688062ee06ca20651ade2d085440aa6
Mohini-git <mohini.sarmokadam12@gmail.com>
2024-07-24 18:04:04 +0530

    first commit Lab001 with addition

---
+++ src/Laboo2.java
@@ -0,0 +1,9 @@
+public class Laboo2 {
+    public static void main(String[] args) {
+       boolean is_married=true;
+       boolean unmarried=false;
+
+        System.out.println(is_married);
+        System.out.println(unmarried);
+    }
+}
