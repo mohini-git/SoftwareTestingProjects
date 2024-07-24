a06fba93d2cf92d149de3c29acd8466c6c157f20
Mohini-git <mohini.sarmokadam12@gmail.com>
2024-07-24 18:32:46 +0530

    commit Lab003 with numerical

---
+++ src/ex_070724/Lab003.java
@@ -0,0 +1,20 @@
+package ex_070724;
+
+public class Lab003 {
+    public static void main(String[] args) {
+       // numerical data-types variables
+        byte a=50;
+        short b = 100;
+        int i = 110;
+        long ph_no = 99005656;
+        String cc = "ABC";
+        String mm = "Numerical";
+
+
+        System.out.println(a + b);
+        System.out.println(i);
+        System.out.println(ph_no);
+        System.out.println(cc+mm);
+      //  System.out.println("ph no-" +long);
+    }
+}
