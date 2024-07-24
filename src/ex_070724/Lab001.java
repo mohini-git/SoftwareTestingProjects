f61ab453a5071c08d638d8aa40c88d6493bb7580
Mohini-git <mohini.sarmokadam12@gmail.com>
2024-07-24 14:56:57 +0530

    first commit Lab001 with addition

--- src/ex_070724/Lab001.java
+++ src/ex_070724/Lab001.java
@@ -2,6 +2,10 @@ package ex_070724;
 
 public class Lab001 {
     public static void main(String[] args) {
-        System.out.printf("Hello World");
+        System.out.println("Hello World");
+        int a, b;
+        a=10;
+        b=++a;
+        System.out.println("b is ->" + b);
     }
 }
